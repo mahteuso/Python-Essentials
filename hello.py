5d49710e648448b4e8cc7fa877bc4c746cf459c7
Mateus <mateus@ufscar.br>
2023-11-23 09:57:18 -0300

    config: first_file

---
+++ hello.py
@@ -0,0 +1,11 @@
+class Hello():
+    def __init__(self, first_name: str, last_name: str) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self):
+        return f'User = [ Hello, {self.first_name} {self.last_name}]'
+
+user = Hello('Mateus', 'Laranjeira')
+print(user)
+    
